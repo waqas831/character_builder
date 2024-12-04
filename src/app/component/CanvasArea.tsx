973133f42f6e41a0817d9d7511ca33a96fd533d8
waqas831 <waqasishaq800@gmail.com>
2024-12-04 11:11:29 +0500

    make emoty canvas

--- src/app/component/CanvasArea.tsx
+++ src/app/component/CanvasArea.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useCallback, useState } from 'react';
 import { useDrop, XYCoord } from 'react-dnd';
-import BaseImage from "../../../public/bg.jpg"
 
 interface DroppedItem {
   id: string;
@@ -19,12 +18,12 @@ interface CanvasAreaProps {
   setDroppedItems: React.Dispatch<React.SetStateAction<DroppedItem[]>>;
 }
 
-const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
+const CanvasArea: React.FC<CanvasAreaProps> = ({ droppedItems, setDroppedItems }) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [selectedItem, setSelectedItem] = useState<DroppedItem | null>(null);
   const [isDragging, setIsDragging] = useState(false);
 
-  const [, drop]: any = useDrop({
+  const [, drop] = useDrop({
     accept: 'character',
     drop: (item: DroppedItem, monitor) => {
       const offset: XYCoord | null = monitor.getClientOffset();
@@ -33,7 +32,7 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
         const rect = canvas.getBoundingClientRect();
         const x = offset.x - rect.left;
         const y = offset.y - rect.top;
-        setDroppedItems((prevItems: any) => [
+        setDroppedItems((prevItems) => [
           ...prevItems,
           { ...item, x, y, width: 80, height: 80, borderRadius: 0, opacity: 1, rotate: 0 },
         ]);
@@ -50,7 +49,7 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
     const y = e.clientY - rect.top;
 
     const item = droppedItems.find(
-      (item: any) => x >= item.x && x <= item.x + item.width && y >= item.y && y <= item.y + item.height
+      (item) => x >= item.x && x <= item.x + item.width && y >= item.y && y <= item.y + item.height
     );
 
     if (item) {
@@ -71,8 +70,8 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    setDroppedItems((prevItems: any) =>
-      prevItems.map((item: any) =>
+    setDroppedItems((prevItems) =>
+      prevItems.map((item) =>
         item.id === selectedItem.id ? { ...item, x, y } : item
       )
     );
@@ -90,9 +89,9 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    setDroppedItems((prevItems: any) =>
+    setDroppedItems((prevItems) =>
       prevItems.filter(
-        (item: any) => !(x >= item.x && x <= item.x + item.width && y >= item.y && y <= item.y + item.height)
+        (item) => !(x >= item.x && x <= item.x + item.width && y >= item.y && y <= item.y + item.height)
       )
     );
   };
@@ -101,8 +100,8 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
     if (!selectedItem) return;
 
     const { name, value } = e.target;
-    setDroppedItems((prevItems: any) =>
-      prevItems.map((item: any) =>
+    setDroppedItems((prevItems) =>
+      prevItems.map((item) =>
         item.id === selectedItem.id ? { ...item, [name]: parseFloat(value) } : item
       )
     );
@@ -118,19 +117,11 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Load default person-like image
-    const defaultImg = new Image();
-    defaultImg.src = "https://i.ibb.co/tsj0rsT/bg.png"; // Replace with actual URL
-    // defaultImg.crossOrigin = 'anonymous'; // This is important for CORS
-    
-    defaultImg.onload = () => {
-      context.drawImage(defaultImg, 0, 0, canvas.width, canvas.height);
-
-      // Draw dropped items
-      droppedItems.forEach((item: any) => {
+    // Draw dropped items
+    if (droppedItems.length > 0) {
+      droppedItems.forEach((item) => {
         const img = new Image();
         img.src = item.imageUrl;
-        // img.crossOrigin = 'anonymous'; // This is important for CORS
         img.onload = () => {
           context.save();
           context.globalAlpha = item.opacity;
@@ -153,7 +144,7 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
           context.restore();
         };
       });
-    };
+    }
   }, [droppedItems]);
 
   useEffect(() => {
@@ -176,20 +167,6 @@ const CanvasArea: React.FC<any> = ({ droppedItems, setDroppedItems }) => {
           height={600}
           className="border-2 border-gray-500 rounded-lg shadow-lg"
         ></canvas>
-       
-      </div>
-      <div>
-         {/* download the image of canvas */}
-       {/* <button className="mt-4 p-2 bg-blue-500 text-white rounded-lg" onClick={()=>{
-          const canvas = canvasRef.current;
-          if (!canvas) return;
-          const link = document.createElement('a');
-          link.download = 'canvas-image.png';
-          link.href = canvas.toDataURL();
-          link.click();
-       }}>
-          Download Image
-        </button> */}
       </div>
       {selectedItem && (
         <div className="p-4 bg-white rounded-lg shadow-lg w-full md:w-1/3">
